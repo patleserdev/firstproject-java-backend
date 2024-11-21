41191ea7af1133e85212493ba2d57d7caa2e2743
patleserdev <leserpat@hotmail.com>
2024-11-21 10:03:05 +0100

    first commit - create project with maven - springboot

---
+++ src/main/java/com/example/firstproject_java_backend/FirstprojectJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstproject_java_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojectJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectJavaBackendApplication.class, args);
+	}
+
+}
